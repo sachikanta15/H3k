abd3a087347c726d53dfd5209eb891f930a6f6ef
sachikanta15 <sachikantaraulchintu@gmail.com>
2024-09-21 04:35:45 +0530

    completed

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -58,5 +58,6 @@ export const isAmin = (req: Request, res: Response, next: NextFunction) => {
   if (req.role !== "ADMIN") {
     return res.status(403).json({ error: "Access denied" });
   }
+  
   next();
 };
