3c5f86eb0a073764bd90c73d8182958f36ec88f6
sachikanta15 <sachikantaraulchintu@gmail.com>
2024-09-22 16:23:54 +0530

    fixed vercel

--- dist/routes/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_1 = require("../controllers/user");
-const router = (0, express_1.Router)();
-router.get("/", user_1.root),
-    router.post("/signup", user_1.signup);
-router.post("/login", user_1.login);
